bd98b7e8e8b2cec5024e6eb24e98cde4acb15c66
채희동 <60247020+Chae-heedong@users.noreply.github.com>
2024-07-21 22:32:40 +0900

    Title: 함수를 이용한 소수 판별, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240721/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,7 +1,9 @@
 def 소수찾기(k):
     j=round(k**0.5)+1
+    if k==1:
+        return False #1은 제외
     if k==2:
-        return Truerue #2는 배제
+        return True #2는 짝수인 유일한 소수라 예외처리 필요
     for i in range(2,j): #뭐로라도 나눠지면 배제
         if k%i==0:
             return False
@@ -16,6 +18,5 @@ c=0
 for i in range(a,b+1):
     if 소수찾기(i):
         c=c+i
-
 
 print(c)
