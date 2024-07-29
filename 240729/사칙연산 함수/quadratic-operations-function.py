3f9350222bdf0fa3f70c571d7f9cc49a6a230738
채희동 <60247020+Chae-heedong@users.noreply.github.com>
2024-07-29 16:35:21 +0900

    Title: 사칙연산 함수, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240729/사칙연산 함수/quadratic-operations-function.py
+++ 240729/사칙연산 함수/quadratic-operations-function.py
@@ -1,2 +1,9 @@
 a=input().split()
-print(f"{a[0]} * {a[2]} = {int(a[0])*int(a[2])}")
+if a[1]=="*":
+    print(f"{a[0]} * {a[2]} = {int(a[0])*int(a[2])}")
+elif a[1]=="+":
+    print(f"{a[0]} + {a[2]} = {int(a[0])+int(a[2])}")
+elif a[1]=="-":
+    print(f"{a[0]} - {a[2]} = {int(a[0])-int(a[2])}")
+elif a[1]=="/":
+    print(f"{a[0]} / {a[2]} = {int(a[0])/int(a[2])}")
