2f5f97747c0df9e12445f917507036e65d29d703
채희동 <60247020+Chae-heedong@users.noreply.github.com>
2025-02-09 20:38:40 +0900

    Title: 최대로 겹치는 구간, Time: 51ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250209/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 250209/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -2,12 +2,12 @@ n = int(input())
 segments = [tuple(map(int, input().split())) for _ in range(n)]
 lists=[]
 
-for i in range(100):
+for i in range(200):
     lists.append(0)
 
 for i in range(0,n):
-    a=segments[i][0]
-    b=segments[i][1]
+    a=segments[i][0]+100
+    b=segments[i][1]+100
     for j in range(a-1,b):
         lists[j]=lists[j]+1
 
