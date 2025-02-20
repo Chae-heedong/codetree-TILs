327b54f8c2e671e4b257a35d7137b8bd44e70a02
채희동 <60247020+Chae-heedong@users.noreply.github.com>
2025-02-20 16:25:07 +0900

    Title: 체크판위에서 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250220/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,33 @@
+R, C = map(int, input().split())
+grid = [list(input().split()) for _ in range(R)]
+cntw=0 # W개수
+ways=0 #가능한 경우의 수 
+# 양끝 같은색 W-R-W W-R-W-R-W 2 4가능
+# 양끝 다른색 W-R 같이 홀수가능
+
+#WBWB BWBW만 가능
+if grid[0][0]==grid[R-1][C-1]:
+    cnt=0
+else:
+    if grid[0][0]=="B": #BWBW의 경우
+        cnt=0
+        for i in range(0,R):
+            for j in range(0,C):
+                #다음 W찾기
+                if grid[i][j]=="W":
+                    for p in range(i+1,R-1):
+                        for q in range(j+1,C-1):
+                            if grid[p][q]=="B":
+                                cnt=cnt+1                           
+    if grid[0][0]=="W": #WBWB의 경우
+        cnt=0
+        for i in range(0,R-2):
+            for j in range(0,C-2):
+                #다음 B찾기
+                if grid[i][j]=="B":
+                    for p in range(i+1,R-1):
+                        for q in range(j+1,C-1):
+                            if grid[p][q]=="W":
+                                cnt=cnt+1
+print(cnt)
+#무조건 대각선으로 넘기때문에 처음껀 R-2 C-2 두번째껀 R-1 C-1
