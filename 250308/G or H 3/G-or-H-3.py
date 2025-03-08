47bdf2df87939558a7b06093d651657878fb99b0
채희동 <60247020+Chae-heedong@users.noreply.github.com>
2025-03-08 14:25:11 +0900

    Title: G or H 3, Time: 88ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250308/G or H 3/G-or-H-3.py
+++ 250308/G or H 3/G-or-H-3.py
@@ -12,7 +12,7 @@ score=0
 scores=[]
 for i in range (0,x2[-1]-k+1):
     for j in range(i,i+k):
-        for p in range(0,len(x)):
+        for p in range(0,k):
             if x[p]==j:
                 if c[p]=='H':
                     score=score+2
