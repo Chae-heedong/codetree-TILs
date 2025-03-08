12ce68296bd243c0cdb1fc68f7dbfa3be4bfc8dc
채희동 <60247020+Chae-heedong@users.noreply.github.com>
2025-03-08 14:24:18 +0900

    Title: G or H 3, Time: 822ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250308/G or H 3/G-or-H-3.py
@@ -0,0 +1,24 @@
+n, k = map(int, input().split())
+x = []
+c = []
+for _ in range(n):
+    pos, char = input().split()
+    x.append(int(pos))
+    c.append(char)
+
+x2=x[::]
+x2.sort()
+score=0
+scores=[]
+for i in range (0,x2[-1]-k+1):
+    for j in range(i,i+k):
+        for p in range(0,len(x)):
+            if x[p]==j:
+                if c[p]=='H':
+                    score=score+2
+                else:
+                    score=score+1
+    scores.append(score)
+    score=0
+scores.sort()
+print(scores[-1]+1)
